b9d8b3b3cafbf760d0d115c785dc103d0f6329c8
bala-murali-k <k.balamurali0701@gmail.com>
2026-02-02 06:57:52 +0530

    Mobile layout has been implemented

--- src/components/items/items.core.component.tsx
+++ src/components/items/items.core.component.tsx
@@ -1,14 +1,18 @@
 // Required imports
 import { useState, useEffect } from 'react'
 
+// Mui imports
+import { Box, IconButton } from '@mui/material'
+
 // Component imports
 import ListItemsComponent from './list/items.list.component.tsx'
+import { AddItemComponent } from './add/items.add.component.tsx'
 
 // Remote imports
 import ItemsRemote from './../../remotes/items/items.remote.module.collection.js'
 
 // Required objects
-const itemRemote = new ItemsRemote() 
+const itemRemote = new ItemsRemote()
 
 export default function ItemsCoreComponent ({}) {
 
@@ -17,43 +21,62 @@ export default function ItemsCoreComponent ({}) {
     data: [],
     loading: false,
   })
+	const [addItems, setAddItems] = useState<boolean>(false)
 
   // Side effects
-  useEffect(() => {
-    searchItems()
-  }, [])
+  // useEffect(() => {
+  //   searchItems()
+  // }, [])
 
-  // Functions
-  async function searchItems() {
-    setItems((prev) => ({ ...prev, loading: true }))
-    let itemsResponse: any
-    try {
-      itemsResponse = await itemRemote?.searchItems()
-      // if (itemsResponse?.status !== 200) {
-      //   const recievedError = new Error(itemsResponse?.message)
-      //   recievedError['status'] = itemsResponse?.status
-      //   throw recievedError
-      // }
-      if (!Boolean(itemsResponse)) {
-        return
-      }
-      setItems((prev) => ({ ...prev, data: itemsResponse }))
-    } catch (err) {
-      console.error(`HTTP REQUEST FAILED - SEARCH ITEMS - CORE - ERROR : ${err}`)
-    } finally {
-      setItems((prev) => ({ ...prev, loading: false }))
-    }
-  }
+	// Functions
+	async function searchItems() {
+		setItems((prev) => ({ ...prev, loading: true }))
+		let itemsResponse: any
+		try {
+			itemsResponse = await itemRemote?.searchItems()
+			// if (itemsResponse?.status !== 200) {
+			//   const recievedError = new Error(itemsResponse?.message)
+			//   recievedError['status'] = itemsResponse?.status
+			//   throw recievedError
+			// }
+			if (!Boolean(itemsResponse)) {
+			return
+		}
+		setItems((prev) => ({ ...prev, data: itemsResponse }))
+		} catch (err) {
+			console.error(`HTTP REQUEST FAILED - SEARCH ITEMS - CORE - ERROR : ${err}`)
+		} finally {
+			setItems((prev) => ({ ...prev, loading: false }))
+		}
+	}
 
-  return (
-    <>
-      <ListItemsComponent
-        InputData={{
-          data: items?.data,
-          loading: items?.loading,
-        }}
-      />
-    </>
-  )
+	return (
+		<Box sx={{ position: 'relative', height: 'calc(100vh - 60px)' }}>
+			<ListItemsComponent
+				InputData={{
+					data: items?.data,
+					loading: items?.loading,
+				}}
+			/>
+			<Box sx={{ pr: 3, pb: 3, position: 'absolute', bottom: 0, right: 0 }}>
+				<IconButton
+					onClick={() => {setAddItems(prev => !prev)}}
+				>
+					<span className="material-symbols-outlined" style={{ fontSize: '25px', color: 'primary.a60' }}>add</span>
+				</IconButton>
+			</Box>
+			{
+				addItems &&
+				<AddItemComponent
+					InputData={{
+						isOpen: addItems,
+					}}
+					InputFunction={{
+						handleDrawerState: (state) => { setAddItems(state) },
+					}}
+				/>
+			}
+		</Box>
+	  )
 }
 
--- src/components/items/list/items.list.component.tsx
+++ src/components/items/list/items.list.component.tsx
@@ -1,28 +1,52 @@
 // Required imports
-import { Box, Typography } from '@mui/material'
+import { Box, Grid, Card, CardContent, Typography } from '@mui/material'
 
 export default function ListItemsComponent ({ InputData }) {
 
-  return (
-    <Box>
-      {
-        InputData?.loading ?
-          <Box>
-            Loading
-          </Box> :
-          Array?.isArray(InputData?.data) && InputData?.data?.length > 0 ?
-          InputData?.data?.map((itemData, index) => (
-            <Box key={`itemID: ${index}`}>
-              <Typography>
-                {itemData?.title ? itemData?.title : ''}
-              </Typography>
-            </Box>
-          )) :
-            <Box>
-              No data in this array
-            </Box>
-      }
-    </Box>
+	const mockData = [
+		{
+			"item_id": 4,
+			"created_at": "2026-01-02T18:06:03.626+00:00",
+			"modifiled_at": null,
+			"title": "First Ever Api Hit",
+			"description": "This is the first hit of the custom api with only title and description",
+			"display_title": null,
+			"content": null,
+			"is_active": true,
+			"is_deleted": false,
+			"item_metadata": null,
+			"created_by": 1,
+			"modified_by": null
+		}
+	]
+
+	return (
+		<Grid container>
+			{
+				InputData?.loading ?
+					<Box>
+						Loading
+					</Box> :
+					// Array?.isArray(InputData?.data) && InputData?.data?.length > 0 ?
+					mockData?.length > 0 ?
+					<Box sx={{ bgcolor: 'red', width: 'inherit', height: 'calc(100vh - 92%)' }}>
+						{
+							mockData?.map((itemData, index) => (
+							  <Card key={`itemID: ${index}`}>
+								<CardContent>
+								  <Typography variant='subtitle1'>
+									{itemData?.title ? itemData?.title : ''}
+								  </Typography>
+								</CardContent>
+							  </Card>
+							))
+						}
+					</Box> :
+					<Box>
+						No data in this array
+					</Box>
+			}
+		</Grid>
   )
 }
 
